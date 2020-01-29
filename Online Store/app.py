4b53d4d71c2691fcb6a80b60fbbda2b2adf4787c
vankoowe <idgamov@gmail.com>
2020-01-29 14:18:09 +0200

    Fixed login and register

--- Online Store/app.py
+++ Online Store/app.py
@@ -1,13 +1,20 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 #from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY _DATABASE_URI'] = 'sqlite:///test.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 #db = SQLAlchemy(app)
 
 @app.route('/')
-def index():
+def register():
     return render_template('register.html')
+ 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return redirect(url_for('register'))
+
+    return render_template('login.html')
 #disable cache si cykni predi da polzwash
 if __name__ == "__main__":
     app.run(debug=True)
